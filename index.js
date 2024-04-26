47e701d3611bbe1fefd2217472e6ad2233f49bb4
shRahat10 <shahriar.haque.1011@gmail.com>
2024-04-27 01:09:26 +0600

    create operation

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    //========================
+    const spotCollection = client.db('trip-topia').collection('tourists-spots');
+
+    app.post('/tourists-spots', async (req, res) => {
+        const newSpot = req.body;
+        const result = await spotCollection.insertOne(newSpot);
+        res.send(result);
+    })
+    //======================== 
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
