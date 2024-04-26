6a13c6dcc21c9893a9773e9e2fc0eb9f969f7d12
shRahat10 <shahriar.haque.1011@gmail.com>
2024-04-27 03:34:58 +0600

    get operation

--- index.js
+++ index.js
@@ -11,47 +11,56 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qu2gduk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m4myeky.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
-    //========================
-    const spotCollection = client.db('trip-topia').collection('tourists-spots');
+        //========================
+        const spotCollection = client.db('trip-topia').collection('tourists-spots');
 
-    app.post('/tourists-spots', async (req, res) => {
-        const newSpot = req.body;
-        const result = await spotCollection.insertOne(newSpot);
-        res.send(result);
-    })
-    //======================== 
+        app.get('/tourists-spots', async (req, res) => {
+            const cursor = spotCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.post('/tourists-spots', async (req, res) => {
+            const newSpot = req.body;
+            console.log(newSpot);
+            const result = await spotCollection.insertOne(newSpot);
+            res.send(result);
+        })
+
+        //========================
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
     res.send('server is running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 })
