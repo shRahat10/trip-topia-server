b9b17d85518f298d64a9a0e01f50c88fb96c6519
shRahat10 <shahriar.haque.1011@gmail.com>
2024-04-27 22:09:17 +0600

    tourist spot delete operation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const {ObjectId} = require('mongodb')
 
 
 // middleware
@@ -71,6 +72,13 @@ async function run() {
             }
       
             const result = await spotCollection.updateOne(filter, spot, options);
+            res.send(result);
+          })
+
+          app.delete('/tourists-spots/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await spotCollection.deleteOne(query);
             res.send(result);
           })
 
